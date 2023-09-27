26bfeb0155637b0937458e22e0af8433d1dc8341
LB-Squishy <laurentleboulbar@gmail.com>
2023-09-27 12:25:32 +0200

    correctif lastEvent reduce + title/cover undefined

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,9 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData()
+  const last = data?.events.reduce((a, b) => a.date > b.date ? a : b);
+  // console.log(last, typeof last?.title, last?.title, last?.cover)
   return <>
     <header>
       <Menu />
@@ -116,13 +118,16 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+        {/* verif de presence title & cover avant EventCard */}
+        {last?.title && last?.cover &&
+          (<EventCard
+            imageSrc={last.cover}
+            title={last.title}
+            date={new Date(last.date)}
+            small
+            label="boom"
+          />)
+        }
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
